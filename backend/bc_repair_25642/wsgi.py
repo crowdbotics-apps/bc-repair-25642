d2780725502dc8aa2330cd38e7c9c43b5c72f08f
Crowdbotics <team@crowdbotics.com>
2021-04-12 07:19:18 +0000

    Update models

--- backend/bc_repair_25642/wsgi.py
+++ backend/bc_repair_25642/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bc_repair_25642.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bc_repair_25642.settings")
 
 application = get_wsgi_application()
